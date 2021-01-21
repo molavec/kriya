81b7babb738570b26ef65fa9f776a89476deffdb
Ignacio <redhotnacho@gmail.com>
2021-01-21 20:20:31 -0300

    Se crea componente KriyaCard

--- src/KriyaCard/index.tsx
+++ src/KriyaCard/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Card } from 'antd';
 
-const KriyaCard: React = () => {
-    return (<div>Hello World</div>)
-}
+const NOMBRE_KRIYA = "NOMBRE DE LA KRIYA blabla";
+const DESCRIPCION_KRIYA = "lorem ipsum jsnald  aslk saj fsdkaj flsjd ksaj dkaj ska...";
+
+const KriyaCard: React = () => (
+    <Card bordered={true} >
+        <h1> { NOMBRE_KRIYA } </h1>
+        <p> { DESCRIPCION_KRIYA } </p>
+    </Card>
+    )
+
 
 export default KriyaCard;
--- src/index.tsx
+++ src/index.tsx
@@ -3,42 +3,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Layout, Row, Col, Card } from 'antd';
-const { Header, Footer, Content} = Layout;
+const { Header, Footer, Content } = Layout;
 import KriyaCard from './KriyaCard';
 
 import "antd/dist/antd.css"
 import "./index.scss"
 
-const NOMBRE_KRIYA = "NOMBRE DE LA KRIYA"
+
 
 ReactDOM.render(
   <div className="App">
     <>
-    <Layout>
+      <Layout>
         <Header>
           <h1> Kriya App </h1>
         </Header>
         <Content>
-        <KriyaCard></KriyaCard>
-        <Row align="top" justify="center">
-          <Col span={12}>
-          <Card bordered={true} >
-            <h1> { NOMBRE_KRIYA } </h1>
-            <p>lorem ipsum jsnald  aslk saj fsdkaj flsjd ksaj dkaj ska...</p>
-          </Card>
-          <Card  >
-            <h1> { NOMBRE_KRIYA } </h1>
-            <p>lorem ipsum jsnald  aslk saj fsdkaj flsjd ksaj dkaj ska...</p>
-          </Card>
-          <Card  >
-            <h1> { NOMBRE_KRIYA } </h1>
-            <p>lorem ipsum jsnald  aslk saj fsdkaj flsjd ksaj dkaj ska...</p>
-          </Card>
-          </Col>
-        </Row>
+          <Row align="top" justify="center">
+            <Col span={12}>
+              <KriyaCard />
+              <KriyaCard />
+              <KriyaCard />
+            </Col>
+          </Row>
         </Content>
         {/*<Footer>Footer</Footer>*/}
-    </Layout>
+      </Layout>
     </>
 
   </div>,
