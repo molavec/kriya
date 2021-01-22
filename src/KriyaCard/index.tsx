9f66199fea698c6f79cce1f082aa3d2f3f92febe
Ignacio <redhotnacho@gmail.com>
2021-01-22 17:27:27 -0300

    Agrega el tipo de funcion-componente typescript

--- src/KriyaCard/index.tsx
+++ src/KriyaCard/index.tsx
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 const NOMBRE_KRIYA = "NOMBRE DE LA KRIYA blabla";
 const DESCRIPCION_KRIYA = "lorem ipsum jsnald  aslk saj fsdkaj flsjd ksaj dkaj ska...";
 
-const KriyaCard: React = () => (
+const KriyaCard: React.FunctionComponent = () => (
     <Card bordered={true} >
         <h1> { NOMBRE_KRIYA } </h1>
         <p> { DESCRIPCION_KRIYA } </p>
