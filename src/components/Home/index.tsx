f01facee564aca870604ad8c729889a9f69e4a5a
Ignacio <redhotnacho@gmail.com>
2021-01-27 15:34:00 -0300

    Squashed commit of the following:
    commit 82f1c0de7d97dcc33c1d977714d00d62ccf781bf
    Author: Ignacio <redhotnacho@gmail.com>
    Date:   Wed Jan 27 15:27:25 2021 -0300
    
        Optimiza código react-router
    
    commit b6b6a3a9daddd1a9806d94baad032b0ba80bb5c3
    Author: Ignacio <redhotnacho@gmail.com>
    Date:   Wed Jan 27 15:20:14 2021 -0300
    
        Se implementa react-router-dom

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 
 import { Layout, Row, Col } from 'antd';
 const { Content } = Layout;
@@ -8,24 +9,30 @@ import KriyaHeader from '../KriyaHeader'
 const Home: React.FunctionComponent = () => (
   <>
     <Layout>
-      <KriyaHeader name="Kriya App Home"/>
+      <KriyaHeader name="Kriya App Home" />
       <Content>
-        
+
         <Row align="top" justify="center">
           <Col span={12}>
-            <KriyaCard name="Este es un nombre" description="esta es una descripción"/>
+            <Link to="/asanas">
+              <KriyaCard name="Este es un nombre" description="esta es una descripción" />
+            </Link>
           </Col>
         </Row>
-        
+
         <Row align="top" justify="center">
           <Col span={12}>
-            <KriyaCard name="Este es un nombre" description="esta es una descripción"/>
+            <Link to="/asanas">
+              <KriyaCard name="Este es un nombre" description="esta es una descripción" />
+            </Link>
           </Col>
         </Row>
-        
+
         <Row align="top" justify="center">
           <Col span={12}>
-            <KriyaCard name="Este es un nombre" description="esta es una descripción"/>
+            <Link to="/asanas">
+              <KriyaCard name="Este es un nombre" description="esta es una descripción" />
+            </Link>
           </Col>
         </Row>
       </Content>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 // index.tsx
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from './components/Home';
 import KriyaView from './components/KriyaView';
@@ -10,10 +11,10 @@ import "./index.scss"
 
 ReactDOM.render(
   <div className="App">
-     <Home /> 
-    
-    {// <KriyaView />
-    }
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/asanas" component={KriyaView} />
+    </Router>
   </div>,
   document.getElementById('root'),
 )
